9ea67bf1208e0c3d9c9d06ff5992c275c8684140
NguyenVietLam <nguyenvietlamgpc@gmail.com>
2026-09-25 15:56:25 +0700

    Cau truc lai thu muc du an NetBeans

---
+++ README.md
@@ -0,0 +1,1 @@
+# btl.ttcsn.nhom5
---
+++ src/main/java/com/mycompany/ttcsn/TTCSN.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.ttcsn;
+
+/**
+ *
+ * @author PC
+ */
+public class TTCSN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
